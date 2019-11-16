4d3baa1ea90d958506b9e66b1ad849c91e7031ce
guoguangyao <396112406@qq.com>
2019-11-16 23:51:32 +0800

    1116 evening study

---
+++ day01/exercise.py
@@ -0,0 +1,24 @@
+#__author__ = "linnum"
+#__time__ = "2019/11/16 11:43 PM"
+
+"""
+day01作业
+1.排除错误
+name = input("请输入姓名：")
+age = input("请输入年龄：")
+print(输入的是：name)
+print(年龄是：age)
+
+2.画出内存图
+count = 100
+sum = count + 5
+count + 0
+
+3.看教程：www.runoob.com
+
+4.看文档：docs.python.org/zh-cn/3/
+
+5.逛社区：www.pythontab.com
+
+
+"""
