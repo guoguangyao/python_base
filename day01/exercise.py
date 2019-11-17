d37280a8d6917a8682cb6eba5fee30d84037ed4e
guoguangyao <396112406@qq.com>
2019-11-17 10:51:34 +0800

    1117 evening study

--- day01/exercise.py
+++ day01/exercise.py
@@ -22,3 +22,9 @@ count + 0
 
 
 """
+
+# 1.排除错误
+name = input("请输入姓名：")
+age = input("请输入年龄：")
+print("姓名是：" + name)
+print("年龄是：" + age)
---
+++ day02/day02.py
@@ -0,0 +1,22 @@
+#__author__ = "linnum"
+#__time__ = "2019/11/17 10:35 AM"
+
+'''
+# del语句 删除
+# 内存引用计数减1，至0则删除内存
+
+del a  删除变量a   内存中的值不一定删除
+
+None 空值对象
+a = None 删除a变量中的内容，接触a与所代表变量的绑定
+其他语言中 会把a变量删除
+
+'''
+
+name01 = "八戒"
+name02 = "悟空"
+name02 = name01
+del name01 # 删除变量name01, 此时打印会报错
+name02 = None   # 为变量name02赋空值，此时打印会输出None
+
+print(name01)
